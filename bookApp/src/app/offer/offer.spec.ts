f6beb53a3b554e9e289d3c5676a519b867aee07e
formation <hei-hong.traing@egis.fr>
2019-04-11 01:03:07 +0200

    add api test

--- bookApp/src/app/offer/offer.spec.ts
+++ bookApp/src/app/offer/offer.spec.ts
@@ -39,7 +39,9 @@ describe('OfferService', () => {
     httpMock = TestBed.get(HttpTestingController);
   });
 
-
+  afterEach(()=>{
+    httpMock.verify();
+  })
   // offer 1
 
   let offers1 = [
@@ -56,7 +58,7 @@ describe('OfferService', () => {
       "sliceValue": 100,
       "value": 12
     }
-  ]
+  ];
 
   it('should subtract total with a value', ()  =>{
     expect(50).toEqual(service.calculMinus(65, 15));
@@ -70,6 +72,33 @@ describe('OfferService', () => {
     expect(65).toEqual(service.calculSlide(65, 12, 100));
   });
 
+  it('should retrieve offer from the API via GET', ()  =>{
+    let offers1 = [
+      {
+        "type": "percentage",
+        "value": 4
+      },
+      {
+        "type": "minus",
+        "value": 15
+      },
+      {
+        "type": "slice",
+        "sliceValue": 100,
+        "value": 12
+      }
+    ]
+    let listIsbn = ['c8fabf68-8374-48fe-a7ea-a00ccd07afff', 'a460afed-e5e7-4e39-a39d-c885c05db861'];
+    service.getOffer(listIsbn).subscribe(offer=>{
+      expect(offer).toEqual(offers1);
+    })
+
+    const request  = httpMock.expectOne('http://henri-potier.xebia.fr/books/c8fabf68-8374-48fe-a7ea-a00ccd07afff,a460afed-e5e7-4e39-a39d-c885c05db861/commercialOffers');
+    expect(request.request.method).toBe('GET');
+
+    request.flush(offers1);
+
+  });
 
 
 });
